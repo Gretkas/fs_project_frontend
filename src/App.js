1c54483c7bcbe5c2de5fc25bc0f128d1261377d3
Sergio Martinez <sergimar@stud.ntnu.no>
2021-05-12 13:11:36 +0200

    added basic header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
 import { BrowserRouter, Route } from "react-router-dom";
 import LandingContainer from "./components/landing/landingContainer";
-
+import Header from "./components/header/header"
 function App() {
   return (
     <BrowserRouter>
-
+      <Header user={true}/>
       <Route path="/" component={LandingContainer}/>
     </BrowserRouter>
   );
