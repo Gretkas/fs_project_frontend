b17de4f42720ba73c04f317b31adba82db201a7e
Sigmund Granaas <sigmundsgranaas@gmail.com>
2021-05-17 13:29:18 +0200

    Set isLogged in to true for timepicker

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import "./App.css";
 import { BrowserRouter, Route } from "react-router-dom";
 import LandingContainer from "./components/landing/landingContainer";
-import Header from "./components/header/header"
-import LoginContainer from "./components/login/loginContainer"
-import RoomsContainer from "./components/rooms/roomsContainer"
+import Header from "./components/header/header";
+import LoginContainer from "./components/login/loginContainer";
+import RoomsContainer from "./components/rooms/roomsContainer";
 import PrivateRoute from "./components/routes/privateroute";
 import AdminRoute from "./components/routes/adminroute";
-import Admin from './components/admin/admin';
+import Admin from "./components/admin/admin";
 import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import { authUser } from "./data/actions/auth";
 import TimeSelectionTable from "./components/timeselection/TimeSelectionTable";
 
-
 const isAdmin = true;
 function App(props) {
-
   useEffect(() => {
     props.authUser();
   }, []);
-
 
   return props.isLoggedIn !== null ? (
     <BrowserRouter>
@@ -29,21 +26,25 @@ function App(props) {
         exact
         path="/"
         component={LandingContainer}
-        isLoggedIn={isLoggedIn}
+        isLoggedIn={props.isLoggedIn}
       />
-      <PrivateRoute exact path="/rooms" component={RoomsContainer} isLoggedIn={props.isLoggedIn}/>  
+      <PrivateRoute
+        exact
+        path="/rooms"
+        component={RoomsContainer}
+        isLoggedIn={props.isLoggedIn}
+      />
       <PrivateRoute
         exact
         path="/timeselection"
         component={TimeSelectionTable}
-        isLoggedIn={isLoggedIn}
+        isLoggedIn={true}
       />
       <AdminRoute exact path="/admin" component={Admin} isAdmin={isAdmin} />
     </BrowserRouter>
-  ): (
+  ) : (
     ""
   );
-
 }
 
 const mapStateToProps = (state) => ({
